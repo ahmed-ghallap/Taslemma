8649e755afcdd4169eaa44089c863d52604ba2c6
ahmed-ghallap <ahmedor759@gmail.com>
2025-12-26 16:10:58 +0200

    fixing updating image on phones

--- src/blocks/EditableImage.jsx
+++ src/blocks/EditableImage.jsx
@@ -19,16 +19,16 @@ export default function EditableImage() {
   };
 
   const ImageView = (
-    <div className="group relative aspect-square cursor-pointer overflow-hidden rounded-full">
+    <div
+      onClick={() => inputRef.current.click()}
+      className="group relative aspect-square cursor-pointer overflow-hidden rounded-full"
+    >
       <img
         className="h-full w-full cursor-pointer rounded-full object-cover transition-all duration-300 ease-in-out hover:scale-110"
         src={logo}
       />
 
-      <div
-        className="motion-bg-out-primary-950/60 motion-duration-200 absolute inset-0 hidden h-full w-full place-items-center group-hover:grid"
-        onClick={() => inputRef.current.click()}
-      >
+      <div className="motion-bg-out-primary-950/60 motion-duration-200 absolute inset-0 hidden h-full w-full place-items-center group-hover:grid">
         <ImageUp className="text-gray-200" size={32} />
       </div>
     </div>
