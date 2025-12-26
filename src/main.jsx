9c98347b7d1ab1645902d593f6b5a85edd3db208
ahmed-ghallap <ahmedor759@gmail.com>
2025-12-26 20:28:13 +0200

    adding container-query-polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const supportsContainerQueries = "container" in document.documentElement.style;
+
+if (!supportsContainerQueries) {
+  import("container-query-polyfill");
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
